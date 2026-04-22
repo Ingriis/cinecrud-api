37e65dd3bcc320d5f2c4ec3c97be7bdc85d56dc0
Ingriis <ingridhg04@gmail.com>
2026-04-21 19:20:55 -0600

    init: estructura base del proyecto

---
+++ src/main/java/com/cine/CineApiApplication.java
@@ -0,0 +1,12 @@
+package com.cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineApiApplication.class, args);
+	}
+}
